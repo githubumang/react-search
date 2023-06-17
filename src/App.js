3a59ffd1ee68b51846fcaff84085523a94a164b3
Umang <umanggg100@gmail.com>
2023-06-17 21:46:10 +0530

    Display the data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+function ProductCategoryRow({category}){
+  return(
+      <tr>
+        <th colSpan="2">{category}</th>
+      </tr>
+  )
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function ProductRow({name,price,stocked}){
+  const pname=stocked?name:<span style={{color:'red'}}>{name}</span>
+  return(
+      <tr>
+        <td>{pname}</td>
+        <td>{price}</td>
+      </tr>
+  )
+}
+
+
+function ProductTable({products}){
+  const Row = [];
+  let lastCategory=null;
+
+  products.forEach((product) => {
+    // console.log(product.category)
+    if(product.category!==lastCategory){
+      Row.push(<ProductCategoryRow category={product.category} key={product.category}/>)
+    }
+    Row.push(
+      <ProductRow name={product.name} price={product.price} key={product.name} stocked={product.stocked}/>
+    )
+    lastCategory=product.category
+  });
+
+  return(
+    <>
+      <table>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+        </thead>
+        <tbody>{Row}</tbody>
+      </table>
+    </>
+  )
+}
+
+function SearchBar(){
+  return(
+    <>
+      <input type="text" placeholder="Search..."/><br/>
+      <label>
+      <input type="checkbox"/>{' '}Only Show Products in Stock<br/>
+      </label>
+    </>
+  )
+}
+
+function FilterableProductTable({products}){
+  return(
+    <>
+    <SearchBar/>
+    <ProductTable products={products}/>
+    </>
   );
 }
 
-export default App;
+const JSONfile=[
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+]
+
+export default function app(){
+  return(
+    <FilterableProductTable products={JSONfile}/>
+  );
+}
